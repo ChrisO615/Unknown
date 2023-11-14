9a9ee2488c325357d125626aedb6d8e4f76d032d
ChrisO615 <150743915+ChrisO615@users.noreply.github.com>
2023-11-14 15:51:16 -0500

    Add files via upload

---
+++ unknown.R
@@ -0,0 +1,10 @@
+nematode <- read.csv("nematode.csv")
+
+unknown <- nematode$Unknown
+
+plot(nematode$Unknown, 
+     breaks = 15,
+     col = "maroon", 
+     xlab = "Number of unknown Nematodes",
+     ylab = "Frequency",
+     main = "unknown Nematodes per Site")
